6cfbb84b07f6e8eeabb679cd381bc05f5b67bb72
robogf <111620315+robogf@users.noreply.github.com>
2022-12-02 03:20:25 -0800

    added an offlinefallback and register route for asset caching

--- client/src-sw.js
+++ client/src-sw.js
@@ -24,7 +24,18 @@ warmStrategyCache({
   strategy: pageCache,
 });
 
+offlineFallback({
+  pageFallback:('/index.html')
+})
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+new StaleWhileRevalidate({
+  cacheName: 'asset-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+  ],
+}));
